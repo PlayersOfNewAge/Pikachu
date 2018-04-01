6afb727c481e005a186178cefe6e3be745439333
GameUx Studios <playofn@gmail.com>
2018-04-01 18:20:24 +0300

    Discord.JS Updates And Embed Messages
    Same thing on header!

--- Pikachu/index.js
+++ Pikachu/index.js
@@ -20,8 +20,39 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
+  if(cmd === `${prefix}sunucubilgi`) {
+
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("Sunucu Bilgileri")
+    .setColor("#15f153")
+    .setThumbnail(sicon)
+    .addField("Sunucu Adı:", message.guild.name)
+    .addField("Oluşturulma Tarihi:", message.guild.createdAt)
+    .addField("Giriş Tarihiniz:", message.member.joinedAt)
+    .addField("Toplam Kullanıcılar:", message.guild.memberCount);
+
+    return message.channel.send(serverembed);
+  }
+
+  // Kısaltmalar
   if(cmd === `sa`)  {
     return message.channel.send("Aleyküm Selam!");
+  }
+  // -----------
+
+  if(cmd === `${prefix}hakkında`) {
+
+    let bicon = bot.user.displayAvatarURL;
+
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Bilgileri")
+    .setColor("#15f153")
+    .setThumbnail(bicon)
+    .addField("Bot Adı:", bot.user.username)
+    .addField("Oluşturulma Tarihi:", bot.user.createdAt);
+
+    return message.channel.send(botembed);
   }
 
 
